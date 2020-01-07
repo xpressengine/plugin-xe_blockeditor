6e95a59b6654985bc05485dbd218ab0b936ccb64
bnu <bnufactory@gmail.com>
2020-01-07 16:07:05 +0900

    볼드저널에 맞게 사이즈 임의 조정

--- components/Editors/Blockeditor.php
+++ components/Editors/Blockeditor.php
@@ -55,9 +55,10 @@ class Blockeditor extends AbstractEditor
             })
         </script>
 DDD;
-        $content = '<div class="write_form_editor" style="width: 100vw; left: 50%; margin-left: -50vw; position: relative;">';
+        $content = '<div class="write_form_editor" style="width: 1460px; position: relative; margin-left: -62px;">';
         $content .= parent::render();
         $content .= '</div>';
+        $content .= '<style>.edit-post-visual-editor .block-editor-block-list__block { margin-left: 0; margin-right: 0; }</style>';
         $content .= $script;
 
         $this->arguments['content'] = str_replace(['&lt;', '&gt;'], ['&amp;lt;', '&amp;gt;'], $this->arguments['content']);
